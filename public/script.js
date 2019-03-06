d4be3d7b3ec5f1b43a10f44e20e5a9a0bf6b6b42
seven-deuce <40258654+seven-deuce@users.noreply.github.com>
2019-03-06 23:04:07 +0400

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,32 @@
+(async function(window, document) {
+
+		//get the data from json file
+    const data = await fetch("public/data.json").then(res => res.json()).catch(err => console.log(err))
+
+    if (document.getElementById("chart")) {
+        const chart = document.getElementById("chart")
+
+        data.forEach(item => {
+            const row = document.createElement("div")
+            let content = document.createElement("p")
+            content.innerHTML = `<b>${item.country}</b> <br>GDP: $${item.gdp} trillion `
+            const bar = document.createElement("span")
+            bar.style.width = calcBarLength(item.gdp) + "px"
+
+            //deploy nodes to HTML
+            row.appendChild(content)
+            row.appendChild(bar)
+            chart.appendChild(row)
+        })
+    }
+
+    function calcBarLength(number) {
+        if (window.innerWidth > 620) {
+            console.log("kkk")
+            return (number * 500) / 20
+        } else {
+            return (number * 300) / 20
+        }
+    }
+
+})(window, document)
